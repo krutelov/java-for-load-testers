097cbadcb487eca3cab2dce62fc8bda35bb07c8d
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-06-20 15:04:39 +0300

    Jar demo.

--- console-comands.sh
+++ console-comands.sh
@@ -1,3 +1,8 @@
 javac -sourcepath .:.. Demo.java
 javac -classpath .. Demo.java
 java -classpath ..:. Demo
+
+jar -cvf my-super-app.jar .
+jar -tf my-super-app.jar
+java -cp my-super-app.jar com.jet.edu.Demo
+java -jar my-super-app.jar
