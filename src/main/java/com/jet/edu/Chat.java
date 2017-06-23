cac4ad3332ad89b782c291a799e3721b15bfd1ce
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-06-23 17:22:29 +0300

    Procedure style demo.

---
+++ src/main/java/com/jet/edu/Chat.java
@@ -0,0 +1,70 @@
+package com.jet.edu;
+
+
+import static com.jet.edu.Chat.start;
+
+/**
+ * Chat application.
+ */
+public class Chat {
+    public static final int PORT;
+
+    static {
+        PORT = 8888;
+    }
+
+    public static void start() {
+        System.out.println(PORT);
+    }
+
+    public static void start(int port) {
+        System.out.println(port);
+    }
+}
+
+class ChatRunner {
+    public static void main(String[] args) {
+        //.....
+        start();
+        start(9999);
+    }
+}
+
+
+
+
+class Человек {
+    public static void сделайРемонт() {
+        вынесиМусор();
+        вынесиМебель();
+
+    }
+
+    private static void вынесиМебель() {
+        System.out.println("Да сколько можно!!!");
+        System.out.println("Уже вынес давно!!!!");
+    }
+
+    private static void вынесиМусор() {
+        System.out.println("Да сколько можно!!!");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/com/acme/edu/AppTest.java
+++ src/test/java/com/acme/edu/AppTest.java
@@ -1,9 +1,5 @@
 package com.acme.edu;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 /**
  * Unit test for simple App.
  */
