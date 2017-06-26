5de433f564de896d185fd3d7450b71b9a92204be
A1 <a1@admin.lan>
2017-06-26 23:06:09 +0300

    Comments created. Private variables used. Methods better naming.

--- src/main/java/com/jet/calc/Calculator.java
+++ src/main/java/com/jet/calc/Calculator.java
@@ -1,48 +1,63 @@
 package com.jet.calc;
 
+/**
+ * Calculator with simple math actions
+ */
 public class Calculator {
+    private static int lastResult = 0;
+    private static int savedValue = 0;
+
+    /**
+     * returns sum of a and b
+     */
     public static int add (int a, int b) {
-        CalculatorApp.memory = a + b;
+        lastResult = a + b;
         return a + b;
     }
 
+    /**
+     * returns difference of a and b
+     */
     public static int sub (int a, int b) {
-        CalculatorApp.memory = a - b;
+        lastResult = a - b;
         return a - b;
     }
 
+    /**
+     * returns multiplication of a and b
+     */
     public static int mult (int a, int b) {
-        CalculatorApp.memory = a * b;
+        lastResult = a * b;
         return a * b;
     }
 
+    /**
+     * returns division a to b
+     */
     public static int div (int a, int b) {
-        CalculatorApp.memory = a / b;
+        lastResult = a / b;
         return a / b;
     }
 
+    /**
+     * Saves last result to lastResult
+     */
     public static void saveToMemory () {
-        CalculatorApp.currentMemory = CalculatorApp.memory;
+        savedValue = lastResult;
     }
 
-    public static void addMemory () {
-        CalculatorApp.currentMemory = CalculatorApp.memory + CalculatorApp.currentMemory;
+    /**
+     * returns sum of last result and
+     */
+    public static void addToMemory() {
+        savedValue = lastResult + savedValue;
     }
 
-    public static int showMemory () {
-        return CalculatorApp.currentMemory;
+    /**
+     * returns sum of a and b
+     */
+    public static int getSavedValue() {
+        return savedValue;
     }
 
 }
-
-
-
-/**
- * +
- * -
- * *
- * /
- *
- * memory
- * memory + result
- */
--- src/main/java/com/jet/calc/CalculatorApp.java
+++ src/main/java/com/jet/calc/CalculatorApp.java
@@ -1,21 +1,18 @@
 package com.jet.calc;
 
+import static com.jet.calc.Calculator.*;
 
+/**
+ * Runner for Calculator class
+ */
 public class CalculatorApp {
-    public static int memory = 0;
-    public static int currentMemory = 0;
 
     public static void main(String[] args) {
 
-        System.out.println(Calculator.add(1,2));
-        System.out.println(Calculator.sub(1,2));
-        Calculator.saveToMemory();
-        System.out.println(Calculator.div(1,2));
-        System.out.println(Calculator.showMemory());
-        System.out.println(Calculator.mult(1,2));
-        System.out.println(Calculator.showMemory());
-        Calculator.addMemory();
-        System.out.println(Calculator.showMemory());
+        System.out.println(add(1,1));
+        System.out.println(getSavedValue());
+        addToMemory();
+        System.out.println(getSavedValue());
 
     }
 }
