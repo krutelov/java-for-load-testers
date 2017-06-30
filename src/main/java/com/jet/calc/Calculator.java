84ab21069606eb48db7cbf845c2817117242b06b
A1 <a1@admin.lan>
2017-06-30 10:29:28 +0300

    Limit to 10 added.

--- src/main/java/com/jet/calc/Calculator.java
+++ src/main/java/com/jet/calc/Calculator.java
@@ -4,60 +4,136 @@ package com.jet.calc;
  * Calculator with simple math actions
  */
 public class Calculator {
-    private static int lastResult = 0;
-    private static int savedValue = 0;
+    private static double lastResult = 0;
+    private static double savedValue = 0;
 
     /**
      * returns sum of a and b
      */
     public static int add (int a, int b) {
-        lastResult = a + b;
-        return a + b;
+        a = limitValue(a);
+        b = limitValue(b);
+        lastResult = limitValue(a + b);
+        return limitValue(a + b);
+    }
+
+    public static double add (double a, double b) {
+        a = limitValue(a);
+        b = limitValue(b);
+        lastResult = limitValue(a + b);
+        return limitValue(a + b);
     }
 
     /**
      * returns difference of a and b
      */
     public static int sub (int a, int b) {
-        lastResult = a - b;
-        return a - b;
+        a = limitValue(a);
+        b = limitValue(b);
+        lastResult = limitValue(a - b);
+        return limitValue(a - b);
+    }
+
+    public static double sub (double a, double b) {
+        a = limitValue(a);
+        b = limitValue(b);
+        lastResult = limitValue(a - b);
+        return limitValue(a - b);
     }
 
     /**
      * returns multiplication of a and b
      */
     public static int mult (int a, int b) {
-        lastResult = a * b;
-        return a * b;
+        a = limitValue(a);
+        b = limitValue(b);
+        lastResult = limitValue(a * b);
+        return limitValue(a * b);
+    }
+
+    public static double mult (double a, double b) {
+        a = limitValue(a);
+        b = limitValue(b);
+        lastResult = limitValue(a * b);
+        return limitValue(a * b);
     }
 
     /**
      * returns division a to b
      */
     public static int div (int a, int b) {
-        lastResult = a / b;
-        return a / b;
+        a = limitValue(a);
+        b = limitValue(b);
+        lastResult = limitValue(a / b);
+        return limitValue(a / b);
+    }
+
+    public static double div (double a, double b) {
+        a = limitValue(a);
+        b = limitValue(b);
+        lastResult = limitValue(a / b);
+        return limitValue(a / b);
     }
 
     /**
      * Saves last result to lastResult
      */
     public static void saveToMemory () {
-        savedValue = lastResult;
+        savedValue = limitValue(lastResult);
     }
 
     /**
      * returns sum of last result and
      */
     public static void addToMemory() {
-        savedValue = lastResult + savedValue;
+        savedValue = limitValue(lastResult + savedValue);
     }
 
     /**
      * returns sum of a and b
      */
-    public static int getSavedValue() {
+    public static double getSavedValue() {
         return savedValue;
+    }
+
+    /**
+     * modulo
+     */
+    public static int modulo (int value){
+        if (value < 0) {
+            value = -value;
+        }
+        return value;
+    }
+
+    public static double modulo (double value){
+        if (value < 0) {
+            value = -value;
+        }
+        return value;
+    }
+
+    /**
+     * limit -10 10
+     */
+    public static int limitValue (int value){
+        if (value < -10) {
+            value = -10;
+        } else if (value > 10) {
+            value = 10;
+        }
+
+        return value;
+    }
+
+    public static double limitValue (double value){
+        if (value < -10) {
+            value = -10;
+        } else if (value > 10) {
+            value = 10;
+        }
+
+        return value;
     }
 
 }
--- src/main/java/com/jet/calc/CalculatorApp.java
+++ src/main/java/com/jet/calc/CalculatorApp.java
@@ -9,10 +9,21 @@ public class CalculatorApp {
 
     public static void main(String[] args) {
 
-        System.out.println(add(1,1));
+        System.out.println("Integers:");
+        System.out.println(add(1,8));
         System.out.println(getSavedValue());
         addToMemory();
         System.out.println(getSavedValue());
+        System.out.println(modulo(-24));
+        System.out.println("\n");
 
+        System.out.println("Float point:");
+        System.out.println(add(1.3,1.9));
+        System.out.println(add(12.3,1.9));
+        System.out.println(getSavedValue());
+        addToMemory();
+        System.out.println(getSavedValue());
+        System.out.println(modulo(-24.12));
+        System.out.println("\n");
     }
 }
